636c856bfcd2ad39ca5aeff250d2e656e47c8d3d
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-26 14:48:34 -0600

    jsp exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,7 +6,14 @@ import java.io.IOException;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
+    private int counter = 0;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        String name = request.getParameter("name");
+        if (name == null){
+            name = "World";
+        }
+        response.getWriter().println("<h1>Hello, " + name + "!</h1>");
+        counter ++;
+        response.getWriter().println("<h3>The count is " + counter + ".</h3>");
     }
 }
